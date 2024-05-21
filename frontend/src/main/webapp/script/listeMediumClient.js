299e852b756c6d471777e9b1744b3d77bd3b0b1d
Romane GOUINEAUD <romanegnd@gmail.com>
2024-05-21 19:13:10 +0200

    MAJ HTML files

--- frontend/src/main/webapp/script/listeMediumClient.js
+++ frontend/src/main/webapp/script/listeMediumClient.js
@@ -7,16 +7,16 @@ $( document ).ready(function() {
     if (statutConnexion) //le client est connecté 
     {
         htmlBouton = '<button id="bouton-historique" class ="bouton-div-haut-droite">Voir historique</button> <button id="bouton-profil-astal" class ="bouton-div-haut-droite">Profil astral</button> <button id="bouton-deconnexion"class ="bouton-div-haut-droite">Log out</button> '; 
-
-
-
+        htmlMedium = '<tr><th> un nom </th></tr>'; 
     }
 
     else 
     {
         htmlBouton = '<button id="bouton-connexion" class ="bouton-div-haut-droite">Connexion</button>'; 
+        htmlMedium = '<tr><th> un nom </th></tr> <tr><th> un autre nom </th></tr>'; 
     }    
     $('#placeBoutons').html(htmlBouton);
+    $('#tabMedium').html(htmlMedium); 
 
   });
 
