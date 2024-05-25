c82daa20f1ef515b3db1f2eb23c97bd5ad1620d5
Lucas Sanchez <lsanchez@insa-lyon.fr>
2024-05-25 20:47:02 +0200

    Ajout statistique

--- frontend/src/main/webapp/script/accueilEmployeConnecte.js
+++ frontend/src/main/webapp/script/accueilEmployeConnecte.js
@@ -2,6 +2,38 @@ var statutConsultation= true;
 let htmlConsulation; 
 
 
+function buildBarChart(container, graphData) {
+
+    Highcharts.chart(container, {
+
+        chart: {
+            type: 'column'
+        },
+        title: {
+            text: 'Top 5 mediums'
+        },
+      
+        xAxis: {
+            categories: graphData.labels
+        },
+        yAxis: {
+            title: {
+                text: 'Nombre de consultations'
+            }
+        },
+        legend: {
+            enabled: false
+        },
+        credits: {
+            enabled: false
+        },
+        series: [{name: "Données", data: graphData.data}]
+    });
+}
+
+
+
+
 $( document ).ready(function() {
     
     
@@ -79,7 +111,73 @@ $( document ).ready(function() {
     .done(function (response) {
         
         console.log(response);
+        var googleMapInstance = null;
+        
+        function makeInfoWindow(title) {
+                return new google.maps.InfoWindow({
+                    content: '<div>Information: ' + title + '</div>'
+                });
+        }
 
+        function attachInfoWindow(marker, infowindow, htmlDescription) {
+            marker.addListener('click', function () {
+
+                infowindow.setContent(htmlDescription);
+                infowindow.open(googleMapInstance, this);
+            });
+        }
+        
+        function initMap() {
+
+            googleMapInstance = new google.maps.Map($('#map')[0], {
+                center: {lat: 45.7601424, lng: 4.8961779},
+                zoom: 13
+            });
+
+
+            generateMarkers();
+        }
+        
+        function generateMarkers() {
+
+            // Petite popup Google Maps
+            var infowindow = makeInfoWindow('');
+
+           
+
+            for (let key in response.coordonnees) {
+
+                var iconImage = null; // marker par défaut
+ 
+                var marker = new google.maps.Marker({
+                    map: googleMapInstance,
+                    position: {lat: response.coordonnees[key].latitude, lng: response.coordonnees[key].longitude},
+                    title: 'Client ',
+                    icon: iconImage
+                });
+
+                attachInfoWindow(
+                        marker, infowindow,
+                        '<div><strong> Un client </strong> <br/>' + 'Incroyable !' + '</div>'
+                        );
+            }
+
+        }
+        
+        initMap();
+        
+        var lineChartData = {labels: [], data:[], nom:[]};
+        var i = 1;
+        for(let key in response.mediums){
+            lineChartData.labels.push("Top : " + i);
+            lineChartData.data.push(response.mediums[key].nbConsultation);
+            lineChartData.nom.push(response.mediums[key].denomination);
+            i++;
+        }
+            
+        
+        buildBarChart('top5', lineChartData);
+        
         
 
     })
@@ -128,3 +226,4 @@ $( document ).ready(function() {
         //window.location.href = "./commentaires.html"; 
     });
 }); 
+
